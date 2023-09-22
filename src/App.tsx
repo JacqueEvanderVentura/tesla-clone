e68bbaeb93c9023970552a00fc6c24c7454c238f
JEVANDER <jacque.evander.ventura@gmail.com>
2023-09-21 21:58:51 -0400

    chore(eslint): eslint added, only level 0 rules set

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-function App() {
+import { ReactNode } from "react";
 
+function App(): ReactNode {
   return (
     <>
+      <h1 className="text-blue-500 text-4xl">holi</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
