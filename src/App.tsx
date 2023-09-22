e918bf6b81450b44d16fae9774a7c4e4711d52da
JEVANDER <jacque.evander.ventura@gmail.com>
2023-09-21 23:56:17 -0400

    chore(prettier): prettier added, [eslint\prettier]ignore added, eslint-config-prettier added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { ReactNode } from "react";
 
 function App(): ReactNode {
-  return (
-    <>
-      <h1 className="text-blue-500 text-4xl">holi</h1>
-    </>
-  );
+	const a = 5;
+	return (
+		<>
+			<h1 className="text-blue-500 text-4xl">holi</h1>
+			{a}
+		</>
+	);
 }
 
 export default App;
