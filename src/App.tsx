b92814385c2c0a3aeee27e55b11d35c10270ba4d
JEVANDER <jacque.evander.ventura@gmail.com>
2023-09-22 00:08:56 -0400

    chore(folder-structure/sass): added sass, updated folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { ReactNode } from "react";
 
 function App(): ReactNode {
-	const a = 5;
 	return (
 		<>
-			<h1 className="text-blue-500 text-4xl">holi</h1>
-			{a}
+			<h1 className="text-blue-500 text-4xl">Write something</h1>
 		</>
 	);
 }
